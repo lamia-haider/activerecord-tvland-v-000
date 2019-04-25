5499117b44e96edc298b04090c784d73f9d6477e
Lamia Haider <lamia.haider@gmail.com>
2019-04-25 10:32:13 +0000

    Automatically backed up by Learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,9 +1,8 @@
-class CreateCharacters < ActiveRecord::Migration[4.2]
+class CreateCharacters < ActiveRecord::Migration[5.1]
   def change
     create_table :characters do |t|
       t.string :name
-      add_foreign_key :show_id
-      add_foreign_key :actor_id
+
     end
   end
 end
