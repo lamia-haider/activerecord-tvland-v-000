e7721c1e571726ae6b49355c1317eeebe823ffe0
Lamia Haider <lamia.haider@gmail.com>
2019-04-25 09:47:49 +0000

    Automatically backed up by Learn

--- db/migrate/004_create_actor.rb
+++
@@ -1,9 +0,0 @@
-class CreateActors < ActiveRecord::Migration[4.2]
-  def change
-    create_table :actors do |t|
-      #primary key of :id is created for us!
-      t.string :first_name
-      t.string :last_name
-    end
-  end
-end
