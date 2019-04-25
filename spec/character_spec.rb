4c081065a074dc5bf1329dbda2596901b0ab64b3
Lamia Haider <lamia.haider@gmail.com>
2019-04-25 10:32:20 +0000

    Automatically backed up by Learn

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -4,7 +4,7 @@ describe Character do
 
   it "belongs to an actor" do
     danny_pudi = Actor.create(first_name: "Danny", last_name: "Pudi")
-    abed = Character.create(name:'Abed', actor_id: danny_pudi.id)
+    abed = Character.create(name:'Abed')
     expect(Character.find_by(:name => "Abed").actor).to eq(danny_pudi)
   end
 
